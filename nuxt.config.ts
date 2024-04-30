3612f415369346c5931faec4da821c06cd19062c
rakibmiah99 <rakibmiah120199@gmail.com>
2024-04-30 15:40:21 +0600

    initial

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    // '@nuxtjs/tailwindcss',
+    '@nuxt/ui',
+    'vue3-carousel-nuxt',
+    '@nuxt/image',
+    '@vueuse/nuxt',
+    // "@sidebase/nuxt-auth",
+  ],
+  css: [
+    '~/assets/css/vendor/video_js.css',
+    '~/assets/css/vendor/plyr.css',
+    '~/assets/css/tailwind.css',
+    '~/assets/css/style.css',
+  ],
+  runtimeConfig: {
+    public: {
+      BASE_URL: process.env.API_BASE_URL
+    }
+  }
+})
